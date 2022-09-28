8897bd9a77227424cf23e529b4e21a0022505121
Darth <redzed427@gmail.com>
2022-09-28 14:16:44 -0400

    Django_course_box_shop_lesson_3_part_2

--- main/settings.py
+++ main/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
